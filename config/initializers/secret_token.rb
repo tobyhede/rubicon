301dcf1aa2e657204c7ae8f5256f5a16e7db2a41
Toby Hede <tobyhede@schrodinger.(none)>
2010-11-10 07:50:34 +1100

    Initial Import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BenchBoss::Application.config.secret_token = '56079f9d3650098b2ca9967a0e8b0edcd4bb89f54ece00eb50e40c6403f4df0305e8469c801c4b9a6ced27ff9e1808303845972b5dcf76c5c34adab0853b2f6f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+BenchBoss::Application.config.session_store :cookie_store, :key => '_BenchBoss_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rake db:sessions:create")
+# BenchBoss::Application.config.session_store :active_record_store
